3397773d9040714915193a8a2651cd97cfdd7692
Mario Ancevski <marioancevski25@gmail.com>
2019-04-13 17:38:15 +0200

    added lab2 problems

---
+++ lab2/CountThree.java
@@ -0,0 +1,90 @@
+package lab2;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CountThree {
+
+    public static int NUM_RUNS = 100;
+    /**
+     * Promenlivata koja treba da go sodrzi brojot na pojavuvanja na elementot 3
+     */
+    int count = 0;
+    /**
+     * TODO: definirajte gi potrebnite elementi za sinhronizacija
+     */
+    static Lock lock;
+    public void init() {
+    	lock = new ReentrantLock();
+    }
+
+    class Counter extends Thread {
+
+        public void count(int[] data) throws InterruptedException {
+            // da se implementira
+        	for(int i=0;i<data.length;i++) {
+        		if(data[i]==3) {
+        			lock.lock();
+        			count++;
+        			lock.unlock();
+        		}
+        	}
+        }
+        private int[] data;
+
+        public Counter(int[] data) {
+            this.data = data;
+        }
+
+        @Override
+        public void run() {
+            try {
+                count(data);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            CountThree environment = new CountThree();
+            environment.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void start() throws Exception {
+
+        init();
+
+        HashSet<Thread> threads = new HashSet<Thread>();
+        Scanner s = new Scanner(System.in);
+        int total=s.nextInt();
+
+        for (int i = 0; i < NUM_RUNS; i++) {
+            int[] data = new int[total];
+            for (int j = 0; j < total; j++) {
+                data[j] = s.nextInt();
+            }
+            Counter c = new Counter(data);
+            threads.add(c);
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+        System.out.println(count);
+
+
+    }
+}
+
